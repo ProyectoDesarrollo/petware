178252c097d656415dd40514eee4239e68627a1c
Roberto <Roberto@192.168.0.157>
2016-01-20 18:25:34 +0100

    20/01/2016 18:25

--- src/Controlador/controlador.java
+++ src/Controlador/controlador.java
@@ -22,8 +22,8 @@ public class controlador implements ActionListener {
     Principal vista;
     String nombrePestaña;
     JTabbedPane tabbedPane;
-   
-    JPanel cliente  = new panelCliente() ;
+
+    JPanel cliente = new panelCliente();
     JPanel producto = new panelCliente();
     JPanel mascota = new panelCliente();
 
@@ -34,7 +34,6 @@ public class controlador implements ActionListener {
 
     public void iniciar() {
 
-        
         this.vista.panelPestaña.setUI(new CustomTabbedPaneUI());
         this.vista.setVisible(true);
         //ActionCommand y ActionListener
@@ -65,90 +64,118 @@ public class controlador implements ActionListener {
         String comand = e.getActionCommand();
 
         if (comand.equals("btnCliente")) {
-            if(this.vista.panelPestaña.indexOfTab("Clientes") <  0){
-              crearPestaña("Clientes");  
-            }else{
-                
-            int ntabCLiente = this.vista.panelPestaña.indexOfTab("Clientes");
-            this.vista.panelPestaña.setSelectedIndex(ntabCLiente);
+            if (this.vista.panelPestaña.indexOfTab("Clientes") < 0) {
+                crearPestaña("Clientes");
+            } else {
+
+                int ntabCLiente = this.vista.panelPestaña.indexOfTab("Clientes");
+                this.vista.panelPestaña.setSelectedIndex(ntabCLiente);
             }
-            
 
         } else if (comand.equals("btnMascotas")) {
-            if(this.vista.panelPestaña.indexOfTab("Mascotas") <  0){
+            if (this.vista.panelPestaña.indexOfTab("Mascotas") < 0) {
                 crearPestaña("Mascotas");
-            }else{
-                  int ntabMascota = this.vista.panelPestaña.indexOfTab("Mascotas");
-                  this.vista.panelPestaña.setSelectedIndex(ntabMascota);
+            } else {
+                int ntabMascota = this.vista.panelPestaña.indexOfTab("Mascotas");
+                this.vista.panelPestaña.setSelectedIndex(ntabMascota);
             }
-            
-          
 
         } else if (comand.equals("btnProductos")) {
-             if(this.vista.panelPestaña.indexOfTab("Productos") <  0){
-                 crearPestaña("Productos");
-             }else{
-                   int ntabProductos = this.vista.panelPestaña.indexOfTab("Productos");
-                   this.vista.panelPestaña.setSelectedIndex(ntabProductos);
-             }
-          
-        
+            if (this.vista.panelPestaña.indexOfTab("Productos") < 0) {
+                crearPestaña("Productos");
+            } else {
+                int ntabProductos = this.vista.panelPestaña.indexOfTab("Productos");
+                this.vista.panelPestaña.setSelectedIndex(ntabProductos);
+            }
 
-        }
-    }
-    public void crearPestaña(String nombre){
-        nombrePestaña = nombre;
-        
-        if(nombrePestaña.equals("Clientes")){
-            this.vista.panelPestaña.addTab(nombrePestaña + "    ",cliente);
-        }else if(nombrePestaña.equals("Mascotas")){
-            this.vista.panelPestaña.addTab(nombrePestaña + "    ",mascota);
-        }else if(nombrePestaña.equals("Productos")){
-            this.vista.panelPestaña.addTab(nombrePestaña + "    ",producto);
-        }
-       
-        
-    }
+        } else if (comand.equals("btnProveedores")) {
+            if (this.vista.panelPestaña.indexOfTab("Proveedores") < 0) {
+                crearPestaña("Proveedores");
+            } else {
+                int ntabProveedores = this.vista.panelPestaña.indexOfTab("Proveedores");
+                this.vista.panelPestaña.setSelectedIndex(ntabProveedores);
+            }
 
-    class CustomTabbedPaneUI extends MetalTabbedPaneUI
-{
-   Rectangle xRect;
-  
-   protected void installListeners() {
-      super.installListeners();
-      tabPane.addMouseListener(new MyMouseHandler());
-   }
-  
-   protected void paintTab(Graphics g, int tabPlacement,
-                           Rectangle[] rects, int tabIndex,
-                           Rectangle iconRect, Rectangle textRect) {
-      super.paintTab(g, tabPlacement, rects, tabIndex, iconRect, textRect);
-  
-      Font f = g.getFont();
-      g.setFont(new Font("Courier", Font.BOLD, 10));
-      FontMetrics fm = g.getFontMetrics(g.getFont());
-      int charWidth = fm.charWidth('x');
-      int maxAscent = fm.getMaxAscent();
-      g.drawString("x", textRect.x + textRect.width - 3, textRect.y + textRect.height - 3);
-      g.drawRect(textRect.x+textRect.width-5,
-                 textRect.y+textRect.height-maxAscent, charWidth+2, maxAscent-1);
-      xRect = new Rectangle(textRect.x+textRect.width-2,
-                 textRect.y+textRect.height-maxAscent, charWidth+2 ,maxAscent-1);
-      g.setFont(f);
-    }
-  
-    public class MyMouseHandler extends MouseAdapter {
-        @Override
-        public void mousePressed(MouseEvent e) {
-            System.out.println(e);
-            if (xRect.contains(e.getPoint())) {
-               JTabbedPane tabPane = (JTabbedPane)e.getSource();
-               int tabIndex = tabForCoordinate(tabPane, e.getX(), e.getY());
-               tabPane.remove(tabIndex);
+        } else if (comand.equals("btnFactura")) {
+            if (this.vista.panelPestaña.indexOfTab("Facturas") < 0) {
+                crearPestaña("Facturas");
+            } else {
+                int ntabFactura = this.vista.panelPestaña.indexOfTab("Facturas");
+                this.vista.panelPestaña.setSelectedIndex(ntabFactura);
+            }
+        }else if ( comand.equals("btnAlimentacion")){
+            if(this.vista.panelPestaña.indexOfTab("Alimentacion")<0){
+                crearPestaña("Alimentacion");
+            }else{
+                int ntabAlimentacion =  this.vista.panelPestaña.indexOfTab("Alimentacion");
+                this.vista.panelPestaña.setSelectedIndex(ntabAlimentacion);
             }
         }
     }
-}
 
+    public void crearPestaña(String nombre) {
+        nombrePestaña = nombre;
+
+        if (nombrePestaña.equals("Clientes")) {
+            this.vista.panelPestaña.addTab(nombrePestaña + "    ", cliente);
+        } else if (nombrePestaña.equals("Mascotas")) {
+            this.vista.panelPestaña.addTab(nombrePestaña + "    ", mascota);
+        } else if (nombrePestaña.equals("Productos")) {
+            this.vista.panelPestaña.addTab(nombrePestaña + "    ", producto);
+
+        } else if (nombrePestaña.equals("Proveedores")) {
+            this.vista.panelPestaña.addTab(nombrePestaña + "    ", producto);
+
+        } else if (nombrePestaña.equals("Facturas")) {
+            this.vista.panelPestaña.addTab(nombrePestaña + "    ", producto);
+
+        } else if (nombrePestaña.equals("Alimentacion")) {
+            this.vista.panelPestaña.addTab(nombrePestaña + "    ", producto);
+        }
+
+    }
+
+    
+    //Metodo para modificar las pestañas del JTabbedPanel
+    class CustomTabbedPaneUI extends MetalTabbedPaneUI {
+
+        Rectangle xRect;
+
+        protected void installListeners() {
+            super.installListeners();
+            tabPane.addMouseListener(new MyMouseHandler());
+        }
+
+        protected void paintTab(Graphics g, int tabPlacement,
+                Rectangle[] rects, int tabIndex,
+                Rectangle iconRect, Rectangle textRect) {
+            super.paintTab(g, tabPlacement, rects, tabIndex, iconRect, textRect);
+
+            Font f = g.getFont();
+            g.setFont(new Font("Courier", Font.BOLD, 10));
+            FontMetrics fm = g.getFontMetrics(g.getFont());
+            int charWidth = fm.charWidth('x');
+            int maxAscent = fm.getMaxAscent();
+            g.drawString("x", textRect.x + textRect.width - 3, textRect.y + textRect.height - 3);
+            g.drawRect(textRect.x + textRect.width - 5,
+                    textRect.y + textRect.height - maxAscent, charWidth + 2, maxAscent - 1);
+            xRect = new Rectangle(textRect.x + textRect.width - 2,
+                    textRect.y + textRect.height - maxAscent, charWidth + 2, maxAscent - 1);
+            g.setFont(f);
+        }
+
+        public class MyMouseHandler extends MouseAdapter {
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println(e);
+                if (xRect.contains(e.getPoint())) {
+                    JTabbedPane tabPane = (JTabbedPane) e.getSource();
+                    int tabIndex = tabForCoordinate(tabPane, e.getX(), e.getY());
+                    tabPane.remove(tabIndex);
+                }
+            }
+        }
+    }
 
 }
