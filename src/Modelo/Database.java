0fa15042616d682d82608374306ea2fbc3637a9a
Alejandro <Alejandro@192.168.1.39>
2016-01-21 21:24:07 +0100

    21:23

--- src/Modelo/Database.java
+++ src/Modelo/Database.java
@@ -11,25 +11,27 @@ import java.sql.SQLException;
 
 public class Database {
     
-    private String db = "DAM01";
+    private String db = "user_dam01_DAM01";
     private String usr = "user_dam01";
     private String pass = "123456";
-    private String url = "jdbc:mysql://90.74.183.48:3306/" + db;
+    private String url = "jdbc:mysql://90.74.183.48:3306" + db;
     private Connection conn = null;
     
     public Database () {
-    
+        
+        this.url = "jdbc:mysql://90.74.183.48:3306:3306/"+this.db;
         try {
             
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection(this.url, this.usr, this.pass);
-            
-        } catch (SQLException | ClassNotFoundException e) {
-            
-            System.err.println(e.getMessage());
-            System.out.println("No va");
-        }
-    
+            System.out.println("Conexion realizada con exito.");
+        }catch(SQLException e){
+            System.out.println("Conexion NO realizada con exito por error de SQL.");
+            System.err.println( e.getMessage() );
+        }catch(ClassNotFoundException e){
+            System.out.println("Conexion NO realizada con exito por error de Class.");
+            System.err.println( e.getMessage() );
+        }   
     }
     
     public Connection getConnection () {
